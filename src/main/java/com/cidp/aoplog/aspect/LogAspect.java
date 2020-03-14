99a23d80d36c153989dec1a781d62d6fd043905a
Zddu <1501815524@qq.com>
2020-03-14 11:51:03 +0800

    整理

--- src/main/java/com/cidp/aoplog/aspect/LogAspect.java
+++ src/main/java/com/cidp/aoplog/aspect/LogAspect.java
@@ -1,6 +1,5 @@
 package com.cidp.aoplog.aspect;
 
-import com.cidp.aoplog.aop.Log;
 import com.cidp.aoplog.service.LogService;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -8,15 +7,12 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 import utils.RequestUtils;
 import utils.SecurityUtils;
 import utils.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Collection;
 
 /**
  * 切面
@@ -29,10 +25,6 @@ public class LogAspect {
     @Autowired
     private LogService logService;
 
-
-//    public LogAspect(LogService logService) {
-//        this.logService = logService;
-//    }
     @Pointcut("@annotation(com.cidp.aoplog.aop.Log)")
     public void logPointcut(){
         // 该方法无方法体,主要为了让同类中其他方法使用此切入点
